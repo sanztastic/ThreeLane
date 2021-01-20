5d498f9c64194059b2d58b2f8c4f19fd229a00e2
Shanand Shrestha <51695785+sanztastic@users.noreply.github.com>
2021-01-20 12:07:13 +0545

    removed extra .

--- js/dataArray.js
+++ js/dataArray.js
@@ -1,41 +1,41 @@
 const imageData = [
     {
-        'src': '../assets/New_vehicle Pack/Ambulance.png',
+        'src': './assets/New_vehicle Pack/Ambulance.png',
         'width': 200,
         'height': 200
     },
     {
-        'src': '../assets/New_vehicle Pack/Police.png',
+        'src': './assets/New_vehicle Pack/Police.png',
         'width': 200,
         'height': 200
     },
     {
-        'src': '../assets/New_vehicle Pack/Black_viper.png',
+        'src': './assets/New_vehicle Pack/Black_viper.png',
         'width': 200,
         'height': 200
     },
     {
-        'src': '../assets/New_vehicle Pack/Car.png',
+        'src': './assets/New_vehicle Pack/Car.png',
         'width': 200,
         'height': 200
     },
     {
-        'src': '../assets/New_vehicle Pack/Mini_truck.png',
+        'src': './assets/New_vehicle Pack/Mini_truck.png',
         'width': 200,
         'height': 200
     },
     {
-        'src': '../assets/New_vehicle Pack/Mini_van.png',
+        'src': './assets/New_vehicle Pack/Mini_van.png',
         'width': 200,
         'height': 200
     },
     {
-        'src': '../assets/New_vehicle Pack/taxi.png',
+        'src': './assets/New_vehicle Pack/taxi.png',
         'width': 200,
         'height': 200
     },
     {
-        'src': '../assets/New_vehicle Pack/truck.png',
+        'src': './assets/New_vehicle Pack/truck.png',
         'width': 260,
         'height': 260
     }
